ada73a9c437973eea6305d22eb08ef4a221d2d75
battaglia-michael <battglia@amazon.com>
2026-04-23 15:04:11 -0400

    Harden notebook instance: add KMS encryption, disable root access

--- terraform/main.tf
+++ terraform/main.tf
@@ -778,6 +778,13 @@ resource "aws_sagemaker_notebook_instance" "notebook" {
   role_arn               = aws_iam_role.sagemaker_execution_role.arn
   default_code_repository = "https://github.com/aws-samples/sample-quantized-ML-model-comparison.git"
   lifecycle_config_name  = aws_sagemaker_notebook_instance_lifecycle_configuration.setup[0].name
+  kms_key_id             = aws_kms_key.sagemaker_endpoint.arn
+  root_access            = "Disabled"
+
+  # VPC placement is not configured for this sample project. The notebook
+  # only needs outbound internet access to invoke SageMaker endpoints and
+  # clone the public GitHub repo. For production use, deploy in a VPC with
+  # VPC endpoints for SageMaker, ECR, and S3.
 
   depends_on = [time_sleep.wait_for_iam]
 
